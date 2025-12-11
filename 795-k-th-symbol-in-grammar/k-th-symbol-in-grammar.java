5e6d8db18f34ee9c7bb031a5fd5aa145584346ba
Asmit45986 <asmit7336@gmail.com>
2025-12-12 00:35:06 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (57.78%) - LeetSync

---
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int kthGrammar(int n, int k) {
+        return solve(n,k);
+    }
+    public static int solve(int n , int k){
+        if(n==1 || k==1){
+            return 0;
+        }
+        int length = (int)Math.pow(2,n-1);
+        int mid = length/2;
+        if(k<=mid){
+            return solve(n-1,k);
+        }else{
+            return 1-solve(n-1,k-mid);
+        }   
+    }
+}
